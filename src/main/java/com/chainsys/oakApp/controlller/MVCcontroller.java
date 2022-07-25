c5036d73cbbaf072740e3a4a5004f6f5c6ecc91c
Saravanakumar <saravanakumar.pandi@chainsys.com>
2022-07-25 12:05:45 +0530

    include MVC controller and views

---
+++ src/main/java/com/chainsys/oakApp/controlller/MVCcontroller.java
@@ -0,0 +1,13 @@
+package com.chainsys.oakApp.controlller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MVCcontroller {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
+
